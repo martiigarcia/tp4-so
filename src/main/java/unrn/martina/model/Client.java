c383056789c55be4a7e2779d6b4fee6aec95041a
marti <martinagarciaa012@gmail.com>
2023-11-17 11:21:58 -0300

    Initial commit

---
+++ src/main/java/unrn/martina/model/Client.java
@@ -0,0 +1,9 @@
+package unrn.martina.model;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Client {
+}
---
+++ src/main/java/unrn/martina/model/Server.java
@@ -0,0 +1,10 @@
+package unrn.martina.model;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Server {
+    
+}
